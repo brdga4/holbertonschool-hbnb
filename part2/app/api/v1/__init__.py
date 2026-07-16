46e808a3eb83c1d90bd7ebbec5e23b126b5ffa74
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-07-16 19:36:34 +0300

    Update __init__ to include Place, User, and Review

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -1,15 +1,19 @@
 from flask import Blueprint
 from flask_restx import Api
+
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.users import api as users_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.places import api as places_ns
 
-
-blueprint = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+blueprint = Blueprint("api_v1", __name__, url_prefix="/api/v1")
 
 api = Api(
-    blueprint,
-    title='HBnB API',
-    version='1.0',
-    description='HBnB Application API'
+    blueprint, title="HBnB API", version="1.0", description="HBnB Application API"
 )
 
-api.add_namespace(amenities_ns, path='/amenities')
+# 2. REGISTER ALL YOUR NAMESPACES
+api.add_namespace(amenities_ns, path="/amenities")
+api.add_namespace(users_ns, path="/users")
+api.add_namespace(reviews_ns, path="/reviews")
+api.add_namespace(places_ns, path="/places")
