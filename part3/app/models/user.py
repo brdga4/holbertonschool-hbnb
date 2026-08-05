a2e385197ed4d508ecdc9860c0856aa1512f7e35
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-05 20:43:27 +0300

    Edit User Model

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,9 +1,20 @@
-from app import bcrypt
+from app import db, bcrypt
 from app.models.base import BaseModel
 
+
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
-        super().__init__()
+    __tablename__ = "users"
+
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __init__(
+        self, first_name, last_name, email, password, is_admin=False, **kwargs
+    ):
+        super().__init__(**kwargs)
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
@@ -11,18 +22,18 @@ class User(BaseModel):
         self.is_admin = is_admin
 
     def hash_password(self, password):
-        return bcrypt.generate_password_hash(password).decode('utf-8')
+        return bcrypt.generate_password_hash(password).decode("utf-8")
 
     def verify_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'is_admin': self.is_admin,
-            'created_at': self.created_at.isoformat(),
-            'updated_at': self.updated_at.isoformat()
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "is_admin": self.is_admin,
+            "created_at": self.created_at.isoformat() if self.created_at else None,
+            "updated_at": self.updated_at.isoformat() if self.updated_at else None,
         }
